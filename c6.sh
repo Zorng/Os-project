bb51470cc75c5aa8a6f25d31196d1428d3a76445
Meakputsoktheara Mony <meakputsoktheara13@gmail.com>
2025-03-26 16:39:29 +0700

    Update c6.sh

--- c6.sh
+++ c6.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# By Mony Meakputsotheara
-function c6() {
-    cd ~
-    echo "Current diretory: $(pwd)" 
-    read -p "Enter file or directory to compress: " target   
-    if [ ! -e "$target" ]; then
-        echo "Error: $target not found"
-        return
-    fi
-    zip -r "${target}.zip" "$target"
+# By Mony Meakputsothera
+read -p "Enter file or directory to compress: " target   
+if [ ! -e "$target" ]; then
+    echo "Error: $target not found"
     return
-}
+fi
+zip -r "${target}.zip" "$target"
+echo "operation succeed"
 
-echo "executing c6"
-c6
+cd ~/bin
+echo "back to ~/bin"
+bash log_action.sh "Compressed $target to ${target}.zip"
