eaec630920d1fc7f549c045e702adc3153cca4d4
Meakputsoktheara Mony <meakputsoktheara13@gmail.com>
2025-03-26 16:39:56 +0700

    Update c5.sh

--- c5.sh
+++ c5.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 # By Mony Meakputsotheara
-function c5() {
-    cd ~
-    echo "Current diretory: $(pwd)" 
-    read -p "Enter directory to search in: " dir
-    if [ ! -e "$dir" ]; then
-        echo "Error: $dir not found"
-        return
-        if [ ! -d "$dir" ]; then
-            echo "Error: $dir is not a directory"
-            return
-        fi
-    fi
-    read -p "Enter filename or extension: " pattern
-    find "$dir" -name "$pattern"
+read -p "Enter directory to search in: " dir
+if [ ! -e "$dir" ]; then
+    echo "Error: ~/$dir not found"
     return
-}
+    if [ ! -d "$dir" ]; then
+        echo "Error: ~/$dir is not a directory"
+        return
+    fi
+fi
+read -p "Enter filename or extension: " pattern
+find "$dir" -name "$pattern"
+echo "operation succeed"
 
-echo "executing c5"
-c5
+cd ~/bin
+echo "back to ~/bin"
+bash log_action.sh "Searched for $pattern in ~/$dir"
