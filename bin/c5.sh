74c52b8ec90be5b23af95217fccf0dff78d541b1
Tong Vorak <vorak.tong@student.cadt.edu.kh>
2025-04-02 21:08:59 +0700

    Update c5.sh

--- bin/c5.sh
+++ bin/c5.sh
@@ -14,5 +14,5 @@ find "$dir" -name "$pattern"
 echo "operation succeed"
 
 cd ~/Os-project-main/bin
-echo "back to ~/bin"
+echo "back to ~/Os-project-main/bin"
 bash log_action.sh "Searched for $pattern in ~/$dir"
